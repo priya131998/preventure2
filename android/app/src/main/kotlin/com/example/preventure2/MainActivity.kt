234faf1af2c81108ad4bc8d21ae614eb68143084
priya131998 <priya.magiya@gmail.com>
2021-10-26 16:55:43 -0700

    style the appbar

---
+++ android/app/src/main/kotlin/com/example/preventure2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.preventure2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Preventure',
+      //add a background color
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFF444442)),
+      home: MyHomePage(title: 'Preventure'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    deviceHeight(BuildContext context) => MediaQuery.of(context).size.height;
+    deviceWidth(BuildContext context) => MediaQuery.of(context).size.width;
+    return Scaffold(
+      //Add an app bar
+      appBar: AppBar(
+        backgroundColor: Color(0xfff5d20f),
+        title: Text(
+          widget.title,
+          style: TextStyle(
+              color: Colors.black.withOpacity(0.8),
+              fontWeight: FontWeight.bold,
+              fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
