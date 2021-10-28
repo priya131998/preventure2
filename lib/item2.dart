2b80bf959e5464acf506063dbb1a1af6e0486bd9
priya131998 <priya.magiya@gmail.com>
2021-10-27 20:35:55 -0700

    create item 2

---
+++ lib/item2.dart
@@ -0,0 +1,261 @@
+import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:preventure2/main.dart';
+import 'ToggleButton.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // Hide the debug banner
+      debugShowCheckedModeBanner: false,
+
+      title: 'Preventure',
+      home: Item2(),
+    );
+  }
+}
+
+class Item2 extends StatelessWidget {
+  static String myVideoId2 = 'SHPcVO0xDrA';
+
+  // Initiate the Youtube player controller
+  YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: myVideoId2,
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: false,
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    deviceHeight(BuildContext context) => MediaQuery.of(context).size.height;
+    deviceWidth(BuildContext context) => MediaQuery.of(context).size.width;
+    return Scaffold(
+      //Add an app bar
+      appBar: AppBar(
+        title: Text("Preventure", style: TextStyle(color: Colors.black)),
+        backgroundColor: Color(0xfff5d20f),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+            size: 30,
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MyHomePage(
+                  title: '',
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: <Widget>[
+              //profile name
+              // SizedBox(
+              //   height: deviceHeight(context) * 0.02,
+              // ),
+              Container(
+                width: deviceWidth(context) * 0.90,
+                // margin: const EdgeInsets.only(bottom: 160.0),
+                child: Text(
+                  'New Feature: Track Slip/Trip/Fall and Lower Limb Injury Risks',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.8), fontSize: 20),
+                ),
+              ),
+              Container(
+                width: deviceWidth(context) * 0.90,
+
+                // margin: const EdgeInsets.only(bottom: 200.0),
+
+                margin: const EdgeInsets.only(top: 20.0),
+                child: Text(
+                  'Date: 02 March 2021',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.8), fontSize: 12.5),
+                ),
+              ),
+              SizedBox(
+                height: deviceHeight(context) * 0.005,
+              ),
+              Container(
+                padding: EdgeInsets.all(15.0),
+                // margin: const EdgeInsets.only(top: 90.0),
+                child: YoutubePlayer(
+                  controller: _controller,
+                  liveUIColor: Colors.amber,
+                ),
+              ),
+              Stack(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          'Mark as watched',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 11.5, fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SizedBox(width: 50.0, height: 20.0, child: Toggle()),
+                      ],
+                    ),
+                    // ],
+                  ),
+                  //),
+                ],
+              ),
+              SizedBox(
+                height: deviceHeight(context) * 0.03,
+              ),
+              // Container(
+              //     margin: const EdgeInsets.only(top: 500.0), child: Toggle()),
+              Container(
+                width: deviceWidth(context) * 0.90,
+                // margin: const EdgeInsets.only(top: 350.0),
+                child: Text(
+                  'We have made it even easier to identify and reduce the risk of ankle, knee and lower back injuries due to',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.8), fontSize: 15),
+                ),
+              ),
+              SizedBox(
+                height: deviceHeight(context) * 0.03,
+              ),
+              Container(
+                width: deviceWidth(context) * 0.90,
+                height: deviceHeight(context) * 0.12,
+                // margin: const EdgeInsets.only(top: 350.0),
+                decoration: BoxDecoration(
+                  color: Color(0xFF444442),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          width: deviceWidth(context) * 0.27,
+                          height: deviceHeight(context) * 0.05,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Color(0xFF444442),
+                                radius: 10,
+                                child: Icon(
+                                  Icons.add,
+                                  color: Color(0xfff5d20f),
+                                  size: 15,
+                                ),
+                              ),
+                              Text('SportsScience',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xfff5d20f),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12.0),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: deviceWidth(context) * 0.27,
+                          height: deviceHeight(context) * 0.05,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Color(0xFF444442),
+                                radius: 10,
+                                child: Icon(
+                                  Icons.add,
+                                  color: Color(0xfff5d20f),
+                                  size: 15,
+                                ),
+                              ),
+                              Text('ProductFeature',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xfff5d20f),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12.0),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: deviceWidth(context) * 0.27,
+                          height: deviceHeight(context) * 0.05,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              CircleAvatar(
+                                backgroundColor: Color(0xFF444442),
+                                radius: 10,
+                                child: Icon(
+                                  Icons.add,
+                                  color: Color(0xfff5d20f),
+                                  size: 15,
+                                ),
+                              ),
+                              Text('InjuryPrevention',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500)),
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xfff5d20f),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12.0),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
